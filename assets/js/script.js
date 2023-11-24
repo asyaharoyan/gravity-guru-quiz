955008fe44e40a948e96e1c4135a33b809179290
asyaharoyan <>
2023-11-24 10:10:30 +0000

    create questions and answers for the game

---
+++ assets/js/script.js
@@ -0,0 +1,162 @@
+const questions = [
+    {
+        question: "Which of the following is an example of a vector quantity?",
+        option1: "Time",
+        option2: "Temperature",
+        option3: "Velocity",
+        option4: "Work",
+        answer: 3
+    },
+    {
+        question: "An object is moving with a constant speed in a circular path. Which of the following statements is true?",
+        option1: "The velocity of the object is constant.",
+        option2: "The acceleration of the object is constant.",
+        option3: "The direction of the velocity of the object is constantly changing.",
+        option4: "None of them is true",
+        answer: 3
+    },
+    {
+        question: "Which of the following statements is true about Newton's First Law of Motion?",
+        option1: "An object at rest will stay at rest unless acted upon by a force",
+        option2: "The acceleration of an object is directly proportional to the net force acting on it",
+        option3: "Every action has an equal and opposite reaction",
+        option4: "Newton is not a physisist",
+        answer: 1
+    },
+    {
+        question: "A ball is thrown horizontally from the top of a cliff with an initial velocity of 10 m/s. If the cliff is 50 meters high, how long will it take for the ball to hit the ground?",
+        option1: "1.4 s",
+        option2: "2.0 s",
+        option3: "5.0 s",
+        option4: "1.8 s",
+        answer: 2
+    },
+    {
+        question: "Which of the following is an example of kinetic energy?",
+        option1: "A stretched spring",
+        option2: "A moving car",
+        option3: "A raised weight",
+        option4: "None",
+        answer: 2
+    },
+    {
+        question: "A car accelerates uniformly from rest to a speed of 25 m/s in 10 seconds. What is the car's acceleration?",
+        option1: "2.5 m/s^2",
+        option2: "10 m/s^2",
+        option3: "25 m/s^2",
+        option4: "13 m/s^2",
+        answer: 1
+    },
+    {
+        question: "Which of the following statements is true about electric charge?",
+        option1: "Like charges attract each other",
+        option2: "Opposite charges repel each other",
+        option3: "Neutral objects can be charged by contact with a charged object",
+        option4: "Charges can not attract or repel",
+        answer: 3
+    },
+    {
+        question: "A 5 kg object is suspended by a string from the ceiling of an elevator. If the elevator is accelerating upwards at a rate of 3 m/s^2, what is the tension in the string?",
+        option1: "20 N",
+        option2: "35 N",
+        option3: "45 N",
+        option4: "50 N",
+        answer: 4
+    },
+    {
+        question: "Which of the following is the SI unit for measuring electric current?",
+        option1: "Joule",
+        option2: "Watt",
+        option3: "Ampere",
+        option4: "N",
+        answer: 3
+    },
+    {
+        question: "Which physicist developed the laws of motion and the law of universal gravitation?",
+        option1: "Benjamin Franklin",
+        option2: "Albert Einstein",
+        option3: "Stephen Hawking",
+        option4: "Isaac Newton",
+        answer: 4
+    },
+    {
+        question: "Which of the following is an example of projectile motion?",
+        option1: "A car driving down a straight road",
+        option2: "A baseball being thrown from pitcher to catcher",
+        option3: "A person walking in a circle",
+        option4: "None of them",
+        answer: 2
+    },
+    {
+        question: "A block with a mass of 2 kg is placed on a horizontal surface with a coefficient of friction of 0.4. A force of 10 N is applied to the block. What is the acceleration of the block?",
+        option1: "2.0 m/s^2",
+        option2: "3.0 m/s^2",
+        option3: "4.0 m/s^2",
+        option4: "5.0 m/s^2",
+        answer: 2
+    },
+    {
+        question: "Which of the following is NOT an example of a vector quantity?",
+        option1: "Acceleration",
+        option2: "Magnetic induction",
+        option3: "Force",
+        option4: "Mass",
+        answer: 4
+    },
+    {
+        question: "Which of the following is an example of Newton's First Law of Motion?",
+        option1: "A soccer ball comes to rest after being kicked across the field.",
+        option2: "A car speeds up when the gas pedal is pressed.",
+        option3: "An object at rest remains at rest unless acted upon by an external force.",
+        option4: "None of them",
+        answer: 3
+    },
+    {
+        question: "A ray of light travels from air to glass at an angle of incidence of 30 degrees. If the refractive index of glass is 1.5, what is the angle of refraction?",
+        option1: "19.5 degrees",
+        option2: "30 degrees",
+        option3: "45 degrees",
+        option4: "48 degrees",
+        answer: 1
+    },
+    {
+        question: "Which of the following is an example of potential energy?",
+        option1: "A ball rolling down a hill",
+        option2: "A stretched rubber band",
+        option3: "A moving car",
+        option4: "A person walking",
+        answer: 2
+    },
+    {
+        question: "A ball is thrown straight up into the air. At the highest point of its trajectory, which of the following statements is true?",
+        option1: "The velocity of the ball is zero.",
+        option2: "The acceleration of the ball is zero.",
+        option3: "The force acting on the ball is zero.",
+        option4: "The force acting on the ball is equal to the balls weight.",
+        answer: 1
+    },
+    {
+        question: "Which of the following is true about the law of conservation of energy?",
+        option1: "Energy cannot be created or destroyed, only transformed from one form to another.",
+        option2: "The total energy of a system remains constant if no external work is done on the system.",
+        option3: "Both a and b are correct.",
+        option4: "Both a and b are incorrect.",
+        answer: 3
+    },
+    {
+        question: "Which physicist developed the theory of general relativity?",
+        option1: "Isaac Newton",
+        option2: "Albert Einstein",
+        option3: "Galileo Galilei",
+        option4: "Thomas Edison",
+        answer: 2
+    },
+    {
+        question: "Which of the following is an example of a non-contact force?",
+        option1: "Magnetic force",
+        option2: "Friction",
+        option3: "Gravity",
+        option4: "Tension",
+        answer: 1
+    }
+];
